bcf07a55e38a77c56cc2e403959536cdc5198c8e
Mohammad Rahimyarza <arza@10-60-49-150.0200.bsd.id.iosda.org>
2018-07-04 14:23:53 +0700

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Mohammad Rahimyarza Bagagarsyah on 04/07/18.
+//  Copyright © 2018 Mohammad Rahimyarza Bagagarsyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
